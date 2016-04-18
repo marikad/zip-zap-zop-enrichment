58ad1a9ed219b22960ebd0719a28d2c8ae47a135
Marika Devan <marikadevan@gmail.com>
2016-04-18 14:55:19 +0100

    updated header

--- wp-content/themes/creativeforces/page-home.php
+++ wp-content/themes/creativeforces/page-home.php
@@ -26,21 +26,21 @@
 <div id="carousel1" class="carousel slide" style="width: 100%;" data-ride="carousel">
 <ol class="carousel-indicators">
 	<li data-target="#carousel1" data-slide-to="0" class="active"></li>
-	<li data-target="#carousel1" data-slide-to="1"></li>
+<!-- 	<li data-target="#carousel1" data-slide-to="1"></li> -->
 </ol>
 <div class="carousel-inner">
 <div class="item active"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/preschool.jpg" alt="" />
 <div class="carousel-caption">
 <div class="inner-caption">
-<h3 class="carousel-head">Advocates For Play</h3>
+<!-- <h3 class="carousel-head">Advocates For Play</h3>
 <p class="carousel-body">Studies show that play is a
 crucial part of a childs development</p>
-<button class="btn btn-primary btn-shadow" type="">Learn More</button>
+<button class="btn btn-primary btn-shadow" type="">Learn More</button> -->
 
 </div>
 </div>
 </div>
-<div class="item"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/kid.jpg" alt="" />
+<!-- <div class="item"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/kid.jpg" alt="" />
 <div class="carousel-caption">
 <div class="inner-caption">
 <h3 class="carousel-head">Advocates For Play</h3>
@@ -50,7 +50,7 @@ crucial part of a childs development</p>
 
 </div>
 </div>
-</div>
+</div> -->
 </div>
  <a class="left carousel-control"  href="#carousel1" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
