a7e08d6678ead3a2f8b70476c2beed72fd90c912
Marika Devan <marikadevan@gmail.com>
2016-04-20 19:46:44 +0100

    changed who we are section

--- wp-content/themes/creativeforces/page-home.php
+++ wp-content/themes/creativeforces/page-home.php
@@ -23,12 +23,7 @@
 		 <?php get_sidebar(); ?>
 </header>
 
-<div id="carousel1" class="carousel slide" style="width: 100%;" data-ride="carousel">
-<ol class="carousel-indicators">
-	<li data-target="#carousel1" data-slide-to="0" class="active"></li>
-<!-- 	<li data-target="#carousel1" data-slide-to="1"></li> -->
-</ol>
-<div class="carousel-inner">
+<!-- <div class="carousel-inner">
 <div class="item active"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/preschool.jpg" alt="" />
 <div class="carousel-caption">
 <div class="inner-caption">
@@ -39,7 +34,7 @@
 </div>
 </div>
 </div>
-<!-- <div class="item"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/kid.jpg" alt="" />
+<div class="item"><img class="img-responsive" style="width: 100%;" src="/wp-content/themes/creativeforces/images/kid.jpg" alt="" />
 <div class="carousel-caption">
 <div class="inner-caption">
 <h3 class="carousel-head">Advocates For Play</h3>
@@ -49,7 +44,7 @@ crucial part of a childs development</p>
 
 </div>
 </div>
-</div> -->
+</div>
 </div>
  <a class="left carousel-control"  href="#carousel1" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -69,13 +64,6 @@ crucial part of a childs development</p>
 <div class="row">
 <div class="space">
 	<p class="mission col-md-6"><strong>Zip Zap Zop Enrichment</strong> provides creative writing and improvisation classes to students in California. Our mission is to empower students with and without special needs by creating inclusive learning opportunities to develop verbal and nonverbal communication skills.</p>
-<!-- <strong>Mission Statement:</strong> Our mission is to empower students with and without special needs by creating inclusive learning opportunities.<br><br>
-<strong>Vision:</strong> Zip Zap Zop Enrichment provides creative writing and improvisation classes to students in California. 
-
-Zip Zap Zop Enrichment aims to develop verbal and nonverbal communication skills through theater improv and creative writing 
-
-Zip Zap Zop Enrichment wishes to unlock the creative potential of students of all abilities and exceptionalities
- -->
 
 <iframe width="560" class="col-md-6" height="315" src="https://www.youtube.com/embed/YzGoBK7X208" frameborder="0" allowfullscreen></iframe>
 </div>
@@ -86,12 +74,6 @@ Zip Zap Zop Enrichment wishes to unlock the creative potential of students of al
 
 <div class="container">
 <p class="text-center program-head">What is Improv?</p>
-<!-- 	<div class="col-md-6 program-item text-center">
-		<img id="image1" class="resize-image img-resonsive col-xs-12" src="/wp-content/themes/creativeforces/images/april2.jpg" alt="" />
-		<p class="program-text">Creative Writing</p>
-		<p class="program-info">this innovative writing program focuses on free thinking and connecting with others thru the written word</p>
-		<button class="btn btn-primary">Learn More</button>
-	</div> -->
 	<div class="row">
 		<div class="col-md-6 program-item text-center">
 		<img id="image1" class="resize-image" src="/wp-content/themes/creativeforces/images/kidsimprov.jpg" alt="" />
@@ -106,7 +88,7 @@ Zip Zap Zop Enrichment wishes to unlock the creative potential of students of al
 
 </section>
 <h3 class="text-center">We have partnered with Zip Zap Zop: Improv for Kids! Click <a href=" http://www.zipzapzopimprov.com/" target="_blank" class="click-here">here</a> to learn more</h3>
-<a href=" http://www.zipzapzopimprov.com/" target="_blank"><img class="center-block" src="/wp-content/themes/creativeforces/images/zip.jpeg" alt="" /></a>
+<a href=" http://www.zipzapzopimprov.com/" target="_blank"><img class="center-block" src="/wp-content/themes/creativeforces/images/zip.jpeg" alt="" /></a> -->
 
     
     
