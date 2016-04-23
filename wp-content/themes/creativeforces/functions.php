ffbc9ac2054025573eca68262b35264d4494bb67
Marika Devan <marikadevan@gmail.com>
2016-04-23 21:39:31 +0100

    updated functions.php

--- wp-content/themes/creativeforces/functions.php
+++ wp-content/themes/creativeforces/functions.php
@@ -1,6 +1,8 @@
+
+
 <?php
 /**
- * Twenty Sixteen functions and definitions
+ * Twenty Fifteen functions and definitions
  *
  * Set up the theme and provides some helper functions, which are used in the
  * theme as custom template tags. Others are attached to action and filter
@@ -21,380 +23,155 @@
  * {@link https://codex.wordpress.org/Plugin_API}
  *
  * @package WordPress
- * @subpackage Creative_Forces
- * @since Creative Forces 1.0
- */
+ * @subpackage Creativeforces
+ * @since Creativeforces 1.0
+ */ 
 
-/**
- * Twenty Sixteen only works in WordPress 4.4 or later.
- */
-if ( version_compare( $GLOBALS['wp_version'], '4.4-alpha', '<' ) ) {
-  require get_template_directory() . '/inc/back-compat.php';
-}
-
-if ( ! function_exists( 'Creativeforces_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- *
- * Create your own twentysixteen_setup() function to override in a child theme.
- *
- * @since Creative Forces 1.0
- */
-function creativeforces_setup() {
-  /*
-   * Make theme available for translation.
-   * Translations can be filed in the /languages/ directory.
-   * If you're building a theme based on Twenty Sixteen, use a find and replace
-   * to change 'twentysixteen' to the name of your theme in all the template files
-   */
-  load_theme_textdomain( 'creativeforces', get_template_directory() . '/languages' );
-
-  // Add default posts and comments RSS feed links to head.
-  add_theme_support( 'automatic-feed-links' );
-
-  /*
-   * Let WordPress manage the document title.
-   * By adding theme support, we declare that this theme does not use a
-   * hard-coded <title> tag in the document head, and expect WordPress to
-   * provide it for us.
-   */
-  add_theme_support( 'title-tag' );
-
-  /*
-   * Enable support for Post Thumbnails on posts and pages.
-   *
-   * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-   */
-  add_theme_support( 'post-thumbnails' );
-  set_post_thumbnail_size( 1200, 9999 );
-
-  // This theme uses wp_nav_menu() in two locations.
-  register_nav_menus( array(
-    'primary' => __( 'Primary Menu', 'creativeforces' ),
-    'social'  => __( 'Social Links Menu', 'creativeforces' ),
-  ) );
-
-  /*
-   * Switch default core markup for search form, comment form, and comments
-   * to output valid HTML5.
-   */
-  add_theme_support( 'html5', array(
-    'search-form',
-    'comment-form',
-    'comment-list',
-    'gallery',
-    'caption',
-  ) );
-
-  /*
-   * Enable support for Post Formats.
-   *
-   * See: https://codex.wordpress.org/Post_Formats
-   */
-  add_theme_support( 'post-formats', array(
-    'aside',
-    'image',
-    'video',
-    'quote',
-    'link',
-    'gallery',
-    'status',
-    'audio',
-    'chat',
-  ) );
-
-  /*
-   * This theme styles the visual editor to resemble the theme style,
-   * specifically font, colors, icons, and column width.
-   */
-  add_editor_style( array( 'css/editor-style.css', creativeforces_fonts_url() ) );
-}
-endif; // twentysixteen_setup
-add_action( 'after_setup_theme', 'creativeforces_setup' );
-
-/**
- * Sets the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- *
- * @since Creative Forces 1.0
- */
-function creativeforces_content_width() {
-  $GLOBALS['content_width'] = apply_filters( 'creativeforces_content_width', 840 );
-}
-add_action( 'after_setup_theme', 'creativeforces_content_width', 0 );
-
-/**
- * Registers a widget area.
- *
- * @link https://developer.wordpress.org/reference/functions/register_sidebar/
- *
- * @since Creative Forces 1.0
- */
-function creativeforces_widgets_init() {
-  register_sidebar( array(
-    'name'          => __( 'Sidebar', 'creativeforces' ),
-    'id'            => 'sidebar-1',
-    'description'   => __( 'Add widgets here to appear in your sidebar.', 'creativeforces' ),
-    'before_widget' => '<section id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
-  ) );
-
-  register_sidebar( array(
-    'name'          => __( 'Content Bottom 1', 'creativeforces' ),
-    'id'            => 'sidebar-2',
-    'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'creativeforces' ),
-    'before_widget' => '<section id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
-  ) );
-
-  register_sidebar( array(
-    'name'          => __( 'Content Bottom 2', 'creativeforces' ),
-    'id'            => 'sidebar-3',
-    'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'creativeforces' ),
-    'before_widget' => '<section id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
-  ) );
-}
-add_action( 'widgets_init', 'creativeforces_widgets_init' );
-
-if ( ! function_exists( 'creativeforces_fonts_url' ) ) :
-/**
- * Register Google fonts for Twenty Sixteen.
- *
- * Create your own twentysixteen_fonts_url() function to override in a child theme.
- *
- * @since Creative Forces 1.0
- *
- * @return string Google fonts URL for the theme.
- */
-function creativeforces_fonts_url() {
-  $fonts_url = '';
-  $fonts     = array();
-  $subsets   = 'latin,latin-ext';
-
-  /* translators: If there are characters in your language that are not supported by Merriweather, translate this to 'off'. Do not translate into your own language. */
-  if ( 'off' !== _x( 'on', 'Merriweather font: on or off', 'creativeforces' ) ) {
-    $fonts[] = 'Merriweather:400,700,900,400italic,700italic,900italic';
-  }
-
-  /* translators: If there are characters in your language that are not supported by Montserrat, translate this to 'off'. Do not translate into your own language. */
-  if ( 'off' !== _x( 'on', 'Montserrat font: on or off', 'creativeforces' ) ) {
-    $fonts[] = 'Montserrat:400,700';
-  }
-
-  /* translators: If there are characters in your language that are not supported by Inconsolata, translate this to 'off'. Do not translate into your own language. */
-  if ( 'off' !== _x( 'on', 'Inconsolata font: on or off', 'creativeforces' ) ) {
-    $fonts[] = 'Inconsolata:400';
-  }
-
-  if ( $fonts ) {
-    $fonts_url = add_query_arg( array(
-      'family' => urlencode( implode( '|', $fonts ) ),
-      'subset' => urlencode( $subsets ),
-    ), 'https://fonts.googleapis.com/css' );
-  }
-
-  return $fonts_url;
-}
-endif;
-
-/**
- * Handles JavaScript detection.
- *
- * Adds a `js` class to the root `<html>` element when JavaScript is detected.
- *
- * @since Creative Forces 1.0
- */
-function creativeforces_javascript_detection() {
-  echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
-}
-add_action( 'wp_head', 'creativeforces_javascript_detection', 0 );
-
-/**
- * Enqueues scripts and styles.
- *
- * @since Creative Forces 1.0
- */
-function creativeforces_scripts() {
-  // Add custom fonts, used in the main stylesheet.
-  wp_enqueue_style( 'creativeforces-fonts', creativeforces_fonts_url(), array(), null );
-
-  // Add Genericons, used in the main stylesheet.
-  wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.4.1' );
-
-  // Theme stylesheet.
-  wp_enqueue_style( 'creativeforces-style', get_stylesheet_uri() );
-
-  // Load the Internet Explorer specific stylesheet.
-  wp_enqueue_style( 'creativeforces-ie', get_template_directory_uri() . '/css/ie.css', array( 'creativeforces-style' ), '20150930' );
-  wp_style_add_data( 'creativeforces-ie', 'conditional', 'lt IE 10' );
-
-  // Load the Internet Explorer 8 specific stylesheet.
-  wp_enqueue_style( 'creativeforces-ie8', get_template_directory_uri() . '/css/ie8.css', array( 'creativeforces-style' ), '20151230' );
-  wp_style_add_data( 'creativeforces-ie8', 'conditional', 'lt IE 9' );
-
-  // Load the Internet Explorer 7 specific stylesheet.
-  wp_enqueue_style( 'creativeforces-ie7', get_template_directory_uri() . '/css/ie7.css', array( 'creativeforces-style' ), '20150930' );
-  wp_style_add_data( 'creativeforces-ie7', 'conditional', 'lt IE 8' );
-
-  // Load the html5 shiv.
-  wp_enqueue_script( 'creativeforces-html5', get_template_directory_uri() . '/js/html5.js', array(), '3.7.3' );
-  wp_script_add_data( 'creativeforces-html5', 'conditional', 'lt IE 9' );
-
-  wp_enqueue_script( 'creativeforces-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151112', true );
-
-  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    wp_enqueue_script( 'comment-reply' );
-  }
-
-  if ( is_singular() && wp_attachment_is_image() ) {
-    wp_enqueue_script( 'creativeforces-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20151104' );
-  }
-
-  wp_enqueue_script( 'creativeforces-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20151204', true );
-
-  wp_localize_script( 'creativeforces-script', 'screenReaderText', array(
-    'expand'   => __( 'expand child menu', 'creativeforces' ),
-    'collapse' => __( 'collapse child menu', 'creativeforces' ),
-  ) );
-}
-add_action( 'wp_enqueue_scripts', 'creativeforces_scripts' );
-
-/**
- * Adds custom classes to the array of body classes.
- *
- * @since Creative Forces 1.0
- *
- * @param array $classes Classes for the body element.
- * @return array (Maybe) filtered body classes.
- */
-function creativeforces_body_classes( $classes ) {
-  // Adds a class of custom-background-image to sites with a custom background image.
-  if ( get_background_image() ) {
-    $classes[] = 'custom-background-image';
-  }
-
-  // Adds a class of group-blog to sites with more than 1 published author.
-  if ( is_multi_author() ) {
-    $classes[] = 'group-blog';
-  }
-
-  // Adds a class of no-sidebar to sites without active sidebar.
-  if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-    $classes[] = 'no-sidebar';
-  }
-
-  // Adds a class of hfeed to non-singular pages.
-  if ( ! is_singular() ) {
-    $classes[] = 'hfeed';
-  }
-
-  return $classes;
-}
-add_filter( 'body_class', 'creativeforces_body_classes' );
-
-/**
- * Converts a HEX value to RGB.
- *
- * @since Creative Forces 1.0
- *
- * @param string $color The original color, in 3- or 6-digit hexadecimal form.
- * @return array Array containing RGB (red, green, and blue) values for the given
- *               HEX code, empty array otherwise.
- */
-function creativeforces_hex2rgb( $color ) {
-  $color = trim( $color, '#' );
-
-  if ( strlen( $color ) === 3 ) {
-    $r = hexdec( substr( $color, 0, 1 ).substr( $color, 0, 1 ) );
-    $g = hexdec( substr( $color, 1, 1 ).substr( $color, 1, 1 ) );
-    $b = hexdec( substr( $color, 2, 1 ).substr( $color, 2, 1 ) );
-  } else if ( strlen( $color ) === 6 ) {
-    $r = hexdec( substr( $color, 0, 2 ) );
-    $g = hexdec( substr( $color, 2, 2 ) );
-    $b = hexdec( substr( $color, 4, 2 ) );
-  } else {
-    return array();
-  }
-
-  return array( 'red' => $r, 'green' => $g, 'blue' => $b );
-}
-
-/**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for content images
- *
- * @since Creative Forces 1.0
- *
- * @param string $sizes A source size value for use in a 'sizes' attribute.
- * @param array  $size  Image size. Accepts an array of width and height
- *                      values in pixels (in that order).
- * @return string A source size value for use in a content image 'sizes' attribute.
- */
-function creativeforces_content_image_sizes_attr( $sizes, $size ) {
-  $width = $size[0];
-
-  840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
-
-  if ( 'page' === get_post_type() ) {
-    840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-  } else {
-    840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
-    600 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-  }
-
-  return $sizes;
-}
-add_filter( 'wp_calculate_image_sizes', 'creativeforces_content_image_sizes_attr', 10 , 2 );
-
-/**
- * Add custom image sizes attribute to enhance responsive image functionality
- * for post thumbnails
- *
- * @since Creative Forces 1.0
- *
- * @param array $attr Attributes for the image markup.
- * @param int   $attachment Image attachment ID.
- * @param array $size Registered image size or flat array of height and width dimensions.
- * @return string A source size value for use in a post thumbnail 'sizes' attribute.
- */
-function creativeforces_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
-  if ( 'post-thumbnail' === $size ) {
-    is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
-    ! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
-  }
-  return $attr;
-}
-add_filter( 'wp_get_attachment_image_attributes', 'creativeforces_post_thumbnail_sizes_attr', 10 , 3 );
-
-/**
- * Modifies tag cloud widget arguments to have all tags in the widget same font size.
- *
- * @since Creative Forces 1.1
- *
- * @param array $args Arguments for tag cloud widget.
- * @return array A new modified arguments.
- */
-function creativeforces_widget_tag_cloud_args( $args ) {
-  $args['largest'] = 1;
-  $args['smallest'] = 1;
-  $args['unit'] = 'em';
-  return $args;
-}
-add_filter( 'widget_tag_cloud_args', 'creativeforces_widget_tag_cloud_args' );
 ?>
 
 
+
+<?php 
+
+require_once('wp_bootstrap_navwalker.php');
+
+
+  // Make theme available for translation
+// Translations can be filed in the /languages/ directory
+load_theme_textdomain( 'creativeforces', TEMPLATEPATH . '/languages' );
+
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if ( is_readable($locale_file) )
+    require_once($locale_file);
+
+// Get the page number
+function get_page_number() {
+    if ( get_query_var('paged') ) {
+        print ' | ' . __( 'Page ' , 'creativeforces') . get_query_var('paged');
+    }
+} // end get_page_number
+
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+       'primary' => __( 'Primary Menu', 'Creativeforces' ),
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' ),
+      'sub_menu' => true
+    )
+  );
+}
+
+add_action( 'init', 'register_my_menus' );
+
+   $defaults = array(
+  'default-image'          => '',
+  'width'                  => 0,
+  'height'                 => 0,
+  'flex-height'            => false,
+  'flex-width'             => false,
+  'uploads'                => false,
+  'random-default'         => false,
+  'header-text'            => true,
+  'default-text-color'     => '',
+  'wp-head-callback'       => '',
+  'admin-head-callback'    => '',
+  'admin-preview-callback' => '',
+);
+add_theme_support( 'custom-header', $defaults );
+
+
+
+function themeslug_theme_customizer( $wp_customize ) {
+
+  $wp_customize->add_section( 'themeslug_logo_section' , array(
+    'title'       => __( 'Logo', 'themeslug' ),
+    'priority'    => 30,
+    'description' => 'Upload a logo to replace the default site name and description in the header',
+) );
+  $wp_customize->add_setting( 'themeslug_logo' );
+
+  $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
+    'label'    => __( 'Logo', 'themeslug' ),
+    'section'  => 'themeslug_logo_section',
+    'settings' => 'themeslug_logo',
+) ) );
+    // Fun code will go here
+}
+add_action( 'customize_register', 'themeslug_theme_customizer' );
+
+ add_filter( 'wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu', 10, 2 );
+// filter_hook function to react on sub_menu flag
+
+
+function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
+  if ( isset( $args->sub_menu ) ) {
+    $root_id = 0;
+    
+    // find the current menu item
+    foreach ( $sorted_menu_items as $menu_item ) {
+      if ( $menu_item->current ) {
+        // set the root id based on whether the current menu item has a parent or not
+        $root_id = ( $menu_item->menu_item_parent ) ? $menu_item->menu_item_parent : $menu_item->ID;
+        break;
+      }
+    }
+    
+    // find the top level parent
+    if ( ! isset( $args->direct_parent ) ) {
+      $prev_root_id = $root_id;
+      while ( $prev_root_id != 0 ) {
+        foreach ( $sorted_menu_items as $menu_item ) {
+          if ( $menu_item->ID == $prev_root_id ) {
+            $prev_root_id = $menu_item->menu_item_parent;
+            // don't set the root_id to 0 if we've reached the top of the menu
+            if ( $prev_root_id != 0 ) $root_id = $menu_item->menu_item_parent;
+            break;
+          } 
+        }
+      }
+    }
+    $menu_item_parents = array();
+    foreach ( $sorted_menu_items as $key => $item ) {
+      // init menu_item_parents
+      if ( $item->ID == $root_id ) $menu_item_parents[] = $item->ID;
+      if ( in_array( $item->menu_item_parent, $menu_item_parents ) ) {
+        // part of sub-tree: keep!
+        $menu_item_parents[] = $item->ID;
+      } else if ( ! ( isset( $args->show_parent ) && in_array( $item->ID, $menu_item_parents ) ) ) {
+        // not part of sub-tree: away with it!
+        unset( $sorted_menu_items[$key] );
+      }
+    }
+    
+    return $sorted_menu_items;
+  } else {
+    return $sorted_menu_items;
+  }
+}
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+  register_sidebar( array(
+    'name'          => 'Primary Sidebar',
+    'id'            => 'home_right_1',
+    'before_widget' => '<div>',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h2 class="rounded">',
+    'after_title'   => '</h2>',
+  ) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
+
+
+
+
+?>
