dc832a379561f8f73adbc03fd6d631861a73a846
Marika Devan <marikadevan@gmail.com>
2016-05-01 00:30:03 +0200

    added press page

---
+++ wp-content/themes/creativeforces/page-media-press.php
@@ -0,0 +1,31 @@
+   
+<?php
+/**
+ * Template Name: Press
+ *
+ * @package WordPress
+ * @subpackage Creativeforces
+ * @since  Creativeforces Group 1.0
+ */
+ ?>
+<head>
+	<meta charset="utf-8">
+	<title>Creative Forces | Press</title>
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/creativeforces/css/press.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+
+
+   <?php get_header(); ?>
+
+
+<div class="text-center">
+<div class="title-article"><h3 class="article-head">Check out the article that was written about us!</h3></div>
+   <img src="/wp-content/themes/creativeforces/images/la-parent.jpg" alt="" />
+   </div>
+   <h3 class="text-center article-head"><a href="https://www.laparent.com/special-needs/zip-zap-zop-fun-ideas-for-kids" target="_blank">Theater Improv Games are Fun Ideas For Kids</a></h3>
+
+
+
+   <?php get_footer(); ?>
