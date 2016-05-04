1447b9ce8231457d17f83045e61050370840b396
Marika Devan <marikadevan@gmail.com>
2016-05-05 00:11:37 +0200

    added new template

--- wp-content/themes/creativeforces/page-about-zacks-blog.php
+++ wp-content/themes/creativeforces/page-about-zacks-blog.php
@@ -11,7 +11,7 @@
  <head>
 	<meta charset="utf-8">
 	<title>Zip Zap Zop Enrichment | Media</title>
-	<!-- <link rel="stylesheet" type="text/css" href="/wp-content/themes/creativeforces/css/media.css"> -->
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/creativeforces/css/blog.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
 </head>
 
