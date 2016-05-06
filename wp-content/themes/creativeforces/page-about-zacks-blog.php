8f0cfb9c219fe7e4568b2445a5fe3e0a6925e5af
Marika Devan <marikadevan@gmail.com>
2016-05-06 19:25:03 +0200

    making blog page

--- wp-content/themes/creativeforces/page-about-zacks-blog.php
+++ wp-content/themes/creativeforces/page-about-zacks-blog.php
@@ -10,7 +10,7 @@
 
  <head>
 	<meta charset="utf-8">
-	<title>Zip Zap Zop Enrichment | Zack's Blogs</title>
+	<title>Zip Zap Zop Enrichment | Media</title>
 	<link rel="stylesheet" type="text/css" href="/wp-content/themes/creativeforces/css/blog.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
 </head>
@@ -28,7 +28,9 @@
 $feed = 'http://miletich2.blogspot.co.uk/feed/';
 $rss = fetch_feed($feed);
 ?>
-<div class="container"> 
+<div class="container">
+<div class="clear">
+<div class="right">      
 <div class="rss-container">
 <div class="rss-heading">
 <?php
@@ -55,11 +57,26 @@ endif;
 ?>
 
 </div>
+</div>
+
+
+
+   <!-- <iframe class="col-md-3"width="560" height="315" src="https://www.youtube.com/embed/89XLwzzcNMA" frameborder="0" allowfullscreen></iframe> -->
+   </div>
 
 </div>
 
   
 
+<!-- 
+<div class="container">
+   <h3 class="text-center article-head">Zip Zap Zop in Action</h3>
+   <div class="row col-md-12">
+   <iframe class="col-md-4" width="560" height="315" src="https://www.youtube.com/embed/8ZGa7X11LI0" frameborder="0" allowfullscreen></iframe>
+   <iframe class="col-md-4" width="560" height="315" src="https://www.youtube.com/embed/MT2pevN9-qo" frameborder="0" allowfullscreen></iframe>
+   <iframe class="col-md-4"width="560" height="315" src="https://www.youtube.com/embed/89XLwzzcNMA" frameborder="0" allowfullscreen></iframe>
+   </div>
+   </div> -->
 
 
    <?php get_footer(); ?>
