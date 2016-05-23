d65921bc7eb14e5ad2538ef62c2325a726e71a80
Marika Devan <marikadevan@gmail.com>
2016-05-23 16:28:40 +0200

    added dropdown for teams

--- wp-content/themes/creativeforces/page-team.php
+++ wp-content/themes/creativeforces/page-team.php
@@ -53,7 +53,7 @@
                         <figure>
                             <img alt="" class="bio-pics" src=
                             "/wp-content/themes/creativeforces/images/april2.jpg"
-                            width="250px">
+                            width="250px" height="250px;">
                         </figure>
                         <h3 class="name">April Miletich-Rasmussen, PhD</h3>
                         <h5 class="job-title">COO</h5>
@@ -62,48 +62,7 @@
                         </div>
                     </div>
                      <div class="line">
-                <h3 class="text-center exec-header">Advisory Board</h3>
-            </div>
-
-            <div class="content-item first cf" id="ari">
-                <figure>
-                    <img alt="" class="bio-pics" src=
-                    "/wp-content/themes/creativeforces/images/ari.jpg" width=
-                    "250px">
-                </figure>
-                <h3 class="name">Ari Schenider</h3>
-                <h5 class="job-title">President</h5>
-                <div class="description">
-<p>Ari Schneider is a graduate of The Second City Conservatory and has a (BA) Hons from The Guildford School of Acting in England. He has been a cast member of the all-ages improv review The Really Awesome Improv Show (Voted Best Kid’s Comedy Show) at The Second City in Hollywood for the past 3 years. He also is affiliated with the mentorship program, YSF (The Young Screenwriters Foundation) at New Rhodes as well as teaching afer-school improv with Zip Zap Zop Enrichment: Non-Profit.</p>
-                </div>
-            </div>
-            <hr>
-            <div class="content-item first cf" id="debra">
-                <figure>
-                    <img alt="" class="bio-pics" src=
-                    "/wp-content/themes/creativeforces/images/debra.jpg" width=
-                    "250px">
-                </figure>
-                <h3 class="name">Debra Gliozzi</h3>
-                <h5 class="job-title">Treasurer</h5>
-                <div class="description">
-<p>Debra Kratochvil Gliozzi is a first generation American and first in her family to attend college. Her career spans 35 years and two distinct industries. Debra is currently an administrator and educator in Danville, California. She brought her MBA and business experience to San Ramon Valley High School and integrated Business Computers (an ROP course), Personal Finance and Introduction to Business and Entrepreneurship into the curriculum. Debra says that her goal is to equip students with skills that prepare them for the real world. It is the most important thing I can do.</p>
-                </div>
-            </div>
-            <hr>
-               <div class="content-item first cf" id="melina">
-                <figure>
-                    <img alt="" class="bio-pics" src=
-                    "/wp-content/themes/creativeforces/images/melina.jpg"
-                    width="250px">
-                </figure>
-                <h3 class="name">Melina Johnson</h3>
-                <h5 class="job-title">Secretary</h5>
-                <div class="description">
-<p>Melina Johnson is a self-employed entrepreneur who created her own home organizing business. Melina is the mother of two children, her son having Autism. She has spent countless hours dedicated to researching and providing her son with the best therapies and services to help him with his growth and development. Every therapist and teacher, over the years, has told Melina that her natural sense of humor has been the best therapy she could provide to her son - Humor and laughter open up doors to cognitive and social development. And it’s fun!</p>
-                </div>
-            </div>
-             </article>
+                
           </div>
        </div>
     </div> 
