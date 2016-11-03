990f87fbf54f9d5f36924841506c78a193356170
Marika Devan <marikadevan@gmail.com>
2016-11-02 20:07:09 -0400

    adding new features

--- wp-content/themes/creativeforces/page-donate.php
+++ wp-content/themes/creativeforces/page-donate.php
@@ -24,10 +24,19 @@
 
 
    <?php get_header(); ?>
+   <div class="text-center">
+   <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="5YM5WAXW9NE2E">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
 
 
 
-<div class="form-container">
+
+<!-- <div class="form-container">
 <div class="donate-heading text-center">
   <h1>Support our Students!</h1>
 </div>
@@ -123,7 +132,7 @@
   </div>
 </form>
 </div>
-  </div>
+  </div> -->
 
 
    <?php get_footer(); ?>
